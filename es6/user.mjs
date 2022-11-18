0e513fdcc70b71d991e4e4010289c005f0d59691
Riba00 <jordiriba@iesebre.com>
2022-11-18 19:59:11 +0100

    Exemple de import

---
+++ es6/user.mjs
@@ -0,0 +1,11 @@
+export default function (e = 'jordiriba@iesebre.com') {
+    let email = e;
+    var password = '1234';
+
+    return {
+        email: email,
+        consoleme: function consoleme() {
+            console.log('Usuari: ' + email)
+        }
+    }
+}
